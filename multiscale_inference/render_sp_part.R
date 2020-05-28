b62274be3e8150f84361e77da69d690ec8b41e77
Lisa Nicvert <lisa.nicvert@insa-lyon.fr>
2020-05-28 14:39:04 +0200

    Launched an example inference

--- multiscale_inference/render_sp_part.R
+++ multiscale_inference/render_sp_part.R
@@ -3,18 +3,17 @@
 # ----------------------------
 
 # ------------------------------------------ Parameters ----------------------------------------
-# folder <- "../filter/filtered_spa/not_annoying/"
-# fname <- "KGA_indep_records_30mins_2.csv"
+folder <- "../data/"
+fname <- "SAM_sample_data.csv"
 
-folder <- "../filter/filtered_spa/not_annoying/seasons/"
-fname <- "MTZ_dry.csv"
-
-th <- 4
+th <- 8
 sel <- "BIC"
-d <- c(lubridate::ddays(20),lubridate::ddays(30),lubridate::ddays(40),lubridate::ddays(60))
+d <- c(lubridate::ddays(10), 
+       lubridate::ddays(15),
+       lubridate::ddays(20))
 
 use_default_offset = FALSE
-habcov = FALSE
+habcov = TRUE
 
 use_hour = FALSE
 hour_offset = "none"
@@ -22,9 +21,10 @@ hour_offset = "none"
 hour_slices <- chron::times(c('00:00:00', '5:00:00', '9:30:00', 
                                 '14:30:00', '19:00:00','23:59:59'))
 spatial_scales <- c("cam",
-                    "clust_fine")
+                    "clust_fine",
+                    "clust_coarse")
 
-sfolder <- "test/"
+sfolder <- "output/"
 
 # ----------------------------------------------------------------------------------------------
 
@@ -87,7 +87,7 @@ render_report = function(file, sfolder,
       spatial_scales = spatial_scales,
       hour_offset = hour_offset
     ),
-    output_file = paste0("space_part_", ncodeloc, "_thr", thr, habcov, offset, hour, "_", selcrit, suffix, ".html"),
+    output_file = paste0("space_part_", ncodeloc, "_thr", thr, habcov, offset, hour, "_", selcrit, suffix, ".pdf"),
     output_dir = saving_folder
   )
 }
@@ -101,14 +101,14 @@ render_report(file = paste0(folder, fname),  sfolder = sfolder,
               use_default_offset = use_default_offset, use_habcov = habcov,
               use_hour = use_hour, hour_slices = hour_slices,
               spatial_scales = spatial_scales, hour_offset = hour_offset)
-          
+
 # --------------------------------------- Threshold choice -------------------------------------
 source("../functions/data_processing.R")
 source("../functions/cooc_classes.R")
 
 # To be run before to choose threshold
 k <- read.csv(paste0(folder, fname),
-                header = TRUE, sep = ",",  colClasses = rep("character", 29))
+                header = TRUE, sep = ",",  colClasses = rep("character", 7))
 # k <- read_files(patterns = c("MAD", "PLN"), path = folder)
 k <- clean_table(k)
 filter_rare_spp(k, filter = FALSE) + geom_hline(yintercept = th)
